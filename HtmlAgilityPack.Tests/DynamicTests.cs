01c7d572dfeb6b090f7ddfbcdd2e52cd545b3713
SND\DarthObiwan_cp <SND\DarthObiwan_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2010-06-05 23:35:39 +0000

    fixed typo in DynamicTests
    git-svn-id: https://htmlagilitypack.svn.codeplex.com/svn/Trunk@66896 81e6e737-5a20-4d0f-a97c-cb01297b46e0

--- HtmlAgilityPack.Tests/DynamicTests.cs
+++ HtmlAgilityPack.Tests/DynamicTests.cs
@@ -10,7 +10,7 @@ namespace HtmlAgilityPack.Tests
         public void TestCallingExistingFunction()
         {
             var doc = new HtmlDocument();
-            doc.LoadHtml("<html><body class=\"asdfasd\"><p>asdf asdf sdf</p></bod></html>");
+            doc.LoadHtml("<html><body class=\"asdfasd\"><p>asdf asdf sdf</p></body></html>");
             dynamic docElement = doc.DocumentNode;
             var item = docElement.Descendants();
             Assert.IsInstanceOf<IEnumerable<HtmlNode>>(item);
@@ -20,32 +20,32 @@ namespace HtmlAgilityPack.Tests
         public void TestCallingExistingMember()
         {
             var doc = new HtmlDocument();
-            doc.LoadHtml("<html><body class=\"asdfasd\"><p>asdf asdf sdf</p></bod></html>");
+            doc.LoadHtml("<html><body class=\"asdfasd\"><p>asdf asdf sdf</p></body></html>");
             dynamic docElement = doc.DocumentNode;
             var item = docElement.Closed;
             Assert.IsInstanceOf<bool>(item);
         }
 
-        [Test]
-        public void TestGetAttribute()
-        {
-            var doc = new HtmlDocument();
-            doc.LoadHtml("<html><body class=\"asdfasd\"><p>asdf asdf sdf</p></bod></html>");
-            dynamic docElement = doc.DocumentNode;
-            var item = docElement.Html.Body._Class;
-            Assert.IsNotNull(item);
-            Assert.IsInstanceOf<HtmlAttribute>(item);
-        }
+[Test]
+public void TestGetAttribute()
+{
+    var doc = new HtmlDocument();
+    doc.LoadHtml("<html><body class=\"asdfasd\"><p>asdf asdf sdf</p></body></html>");
+    dynamic docElement = doc.DocumentNode;
+    var item = docElement.Html.Body._Class;
+    Assert.IsNotNull(item);
+    Assert.IsInstanceOf<HtmlAttribute>(item);
+}
 
-        [Test]
-        public void TestGetMember()
-        {
-            var doc = new HtmlDocument();
-            doc.LoadHtml("<html><body><p>asdf asdf sdf</p></bod></html>");
-            dynamic docElement = doc.DocumentNode;
-            var item = docElement.Html.Body;
-            Assert.IsNotNull(item);
-            Assert.IsInstanceOf<HtmlNode>(item);
-        }
+[Test]
+public void TestGetMember()
+{
+    var doc = new HtmlDocument();
+    doc.LoadHtml("<html><body><p>asdf asdf sdf</p></body></html>");
+    dynamic docElement = doc.DocumentNode;
+    var item = docElement.Html.Body;
+    Assert.IsNotNull(item);
+    Assert.IsInstanceOf<HtmlNode>(item);
+}
     }
 }
