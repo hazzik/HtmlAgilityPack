ca884f70058eb853a65d25bebc1de226692e4910
SND\DarthObiwan_cp <SND\DarthObiwan_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2010-06-07 00:27:36 +0000

    Updates for HAPDynamic to create HtmlNodes and HtmlAttributes if they do not exist when they are invoked dynamically. Currently code is commented out.
    git-svn-id: https://htmlagilitypack.svn.codeplex.com/svn/Trunk@67069 81e6e737-5a20-4d0f-a97c-cb01297b46e0

--- HtmlAgilityPack/HtmlNode.Dynamic.cs
+++ HtmlAgilityPack/HtmlNode.Dynamic.cs
@@ -1,30 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq.Expressions;
-using System.Text;
+﻿using System.Dynamic;
 
 namespace HtmlAgilityPack
 {
     public partial class HtmlNode : DynamicObject
     {
-        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
-        {
-            return base.TryInvokeMember(binder, args, out result);
-        }
-        public override bool TrySetMember(SetMemberBinder binder, object value)
-        {
-
-            return base.TrySetMember(binder, value);
-        }
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
-            if (binder.Name.StartsWith("_"))
-                result = Attributes[binder.Name.Substring(1)];
-            else
-                result = ChildNodes[binder.Name];
+            switch (GetBinderType(binder.Name))
+            {
+                case HtmlNodeType.Attribute:
+                    var name = binder.Name.Substring(1);
+                    result = Attributes[name];
+                    //if (result == null)
+                    //    result = Attributes.Add(name, string.Empty);
+                    break;
+                default:
+                    result = ChildNodes[binder.Name];
+                    //if (result == null)
+                    //{
+                    //    result = new HtmlNode(binder.Name, OwnerDocument);
+                    //    ChildNodes.Add(result as HtmlNode);
+                    //}
+                    break;
+            }
 
             return result != null;
+        }
+
+        private HtmlNodeType GetBinderType(string name)
+        {
+            return name.StartsWith("_") ? HtmlNodeType.Attribute : HtmlNodeType.Element;
         }
     }
 
