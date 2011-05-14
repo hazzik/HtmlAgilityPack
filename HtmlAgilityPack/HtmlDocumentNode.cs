fde9fde012ac832715ccd720eb8a23b694b028c2
hazzik <hazzik@gmail.com>
2011-05-15 01:50:52 +0600

    HtmlElementNodeBase - method WriteTo pulled up to child classes

--- HtmlAgilityPack/HtmlDocumentNode.cs
+++ HtmlAgilityPack/HtmlDocumentNode.cs
@@ -1,10 +1,60 @@
 namespace HtmlAgilityPack
 {
+    using System.IO;
+
     internal class HtmlDocumentNode : HtmlElementNodeBase
     {
         public HtmlDocumentNode(HtmlDocument ownerdocument, int index)
             : base(HtmlNodeType.Document, ownerdocument, index)
         {
+        }
+
+        /// <summary>
+        /// Saves the current node to the specified TextWriter.
+        /// </summary>
+        /// <param name="outText">The TextWriter to which you want to save.</param>
+        public override void WriteTo(TextWriter outText)
+        {
+            if (_ownerdocument.OptionOutputAsXml)
+            {
+#if SILVERLIGHT || PocketPC
+                outText.Write("<?xml version=\"1.0\" encoding=\"" + _ownerdocument.GetOutEncoding().WebName +
+                                             "\"?>");
+#else
+                        outText.Write("<?xml version=\"1.0\" encoding=\"" + _ownerdocument.GetOutEncoding().BodyName +
+                                      "\"?>");
+#endif
+                // check there is a root element
+                if (_ownerdocument.DocumentNode.HasChildNodes)
+                {
+                    int rootnodes = _ownerdocument.DocumentNode._childnodes.Count;
+                    if (rootnodes > 0)
+                    {
+                        HtmlNode xml = _ownerdocument.GetXmlDeclaration();
+                        if (xml != null)
+                            rootnodes --;
+
+                        if (rootnodes > 1)
+                        {
+                            if (_ownerdocument.OptionOutputUpperCase)
+                            {
+                                outText.Write("<SPAN>");
+                                WriteContentTo(outText);
+                                outText.Write("</SPAN>");
+                            }
+                            else
+                            {
+                                outText.Write("<span>");
+                                WriteContentTo(outText);
+                                outText.Write("</span>");
+                            }
+                            return;
+                        }
+                    }
+                }
+            }
+            WriteContentTo(outText);
+            return;
         }
     }
 }
--- HtmlAgilityPack/HtmlElementNode.cs
+++ HtmlAgilityPack/HtmlElementNode.cs
@@ -2,11 +2,88 @@ using System;
 
 namespace HtmlAgilityPack
 {
+    using System.IO;
+
     internal class HtmlElementNode : HtmlElementNodeBase
     {
         public HtmlElementNode(HtmlDocument ownerdocument, int index)
             : base(HtmlNodeType.Element, ownerdocument, index)
         {
+        }
+
+        /// <summary>
+        /// Saves the current node to the specified TextWriter.
+        /// </summary>
+        /// <param name="outText">The TextWriter to which you want to save.</param>
+        public override void WriteTo(TextWriter outText)
+        {
+            string name = _ownerdocument.OptionOutputUpperCase ? Name.ToUpper() : Name;
+            if (_ownerdocument.OptionOutputOriginalCase)
+                name = OriginalName;
+            if (_ownerdocument.OptionOutputAsXml)
+            {
+                if (name.Length > 0)
+                {
+                    if (name[0] == '?')
+                        // forget this one, it's been done at the document level
+                        return;
+
+                    if (name.Trim().Length == 0)
+                        return;
+                    name = HtmlDocument.GetXmlName(name);
+                }
+                else
+                    return;
+            }
+            outText.Write("<" + name);
+            WriteAttributes(outText, false);
+            if (HasChildNodes)
+            {
+                outText.Write(">");
+                bool cdata = false;
+                if (_ownerdocument.OptionOutputAsXml && IsCDataElement(Name))
+                {
+                    // this code and the following tries to output things as nicely as possible for old browsers.
+                    cdata = true;
+                    outText.Write("\r\n//<![CDATA[\r\n");
+                }
+
+
+                if (cdata)
+                {
+                    if (HasChildNodes)
+                        // child must be a text
+                        ChildNodes[0].WriteTo(outText);
+
+                    outText.Write("\r\n//]]>//\r\n");
+                }
+                else
+                    WriteContentTo(outText);
+
+                outText.Write("</" + name);
+                if (!_ownerdocument.OptionOutputAsXml)
+                    WriteAttributes(outText, true);
+
+                outText.Write(">");
+            }
+            else
+            {
+                if (IsEmptyElement(Name))
+                {
+                    if ((_ownerdocument.OptionWriteEmptyNodes) || (_ownerdocument.OptionOutputAsXml))
+                        outText.Write(" />");
+                    else
+                    {
+                        if (Name.Length > 0 && Name[0] == '?')
+                            outText.Write("?");
+
+                        outText.Write(">");
+                    }
+                }
+                else
+                    outText.Write("></" + name + ">");
+            }
+            return;
         }
     }
 }
